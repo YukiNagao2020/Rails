b17303a6b690149d324f9270ea07482c10211786
adachi <yuki.n0301@gmail.com>
2021-07-02 05:58:21 +0000

    [Finish]2-7]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '53c964ccccd1c8654310303139476a0bb7676ad5c9472f5c6c06a5047d7dbc9df2ea0b053baf6d7c866c3e5a6d63e0656b285cc131f238a778be567831736ab2'
